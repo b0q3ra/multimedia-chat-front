ba7b06b81a3291a72785970921e1230ade609419
sergi <sergiboquera@gmail.com>
2022-02-18 13:01:33 +0100

    initial setup. Chat, Login and Register pages layout finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
+import Chat from './pages/Chat/Chat'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Routes>
+
+            {/*Login Route */}
+            <Route path='/login' element={<Login />}/>
+
+            {/*Register Route */}
+            <Route path='/register' element={<Register />}/>
+
+            {/*Default Route */}
+            <Route path='/chat' element={<Chat />}/>
+
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
